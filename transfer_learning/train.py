b6e0705c03dfa1a6d7bdaa5150818dbbb4b2ca62
Michael Schott <misc00001@stud.uni-saarland.de>
2026-08-04 09:42:38 +0200

    Allow setting results directory from cli and yaml

--- transfer_learning/train.py
+++ transfer_learning/train.py
@@ -1,6 +1,6 @@
 from lightning.pytorch import LightningModule, Trainer
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
-from lightning.pytorch.cli import LightningCLI, SaveConfigCallback
+from lightning.pytorch.cli import LightningArgumentParser, LightningCLI, SaveConfigCallback
 from mldb.store import RunStore
 
 from transfer_learning.data import ClassificationDataModule, SegmentationDataModule
@@ -8,13 +8,22 @@ from transfer_learning.modules import ClassificationModule, SegmentationModule
 
 
 class MicrostructuresCLI(LightningCLI):
+    def add_arguments_to_parser(self, parser: LightningArgumentParser) -> None:
+        parser.add_argument(
+            "--results_dir",
+            type=str,
+            default=None,
+            help="Root directory for mldb run storage. Falls back to RunStore.from_env() if unset.",
+        )
+
     def before_instantiate_classes(self) -> None:
         """Points the trainer's TensorBoardLogger at a fresh mldb run directory."""
-        self.store = RunStore.from_env()
+        config = self.config[self.subcommand] if self.subcommand else self.config
+        results_dir = config.results_dir
+        self.store = RunStore(root_dir=results_dir) if results_dir is not None else RunStore.from_env()
         self.run_id = self.store.create_run()
         uri = self.store.open_directory(self.run_id)
 
-        config = self.config[self.subcommand] if self.subcommand else self.config
         config.trainer.logger = {
             "class_path": "lightning.pytorch.loggers.TensorBoardLogger",
             "init_args": {"save_dir": uri, "name": "", "version": ""},
