33d98419d7ef8f7b0abc7a4647f5d2ee7007df36
Michael Schott <misc00001@stud.uni-saarland.de>
2026-08-07 23:36:49 +0200

    Make pods exit upon completion, enable ssh access

--- scripts/tuning/tune_classification_1.sh
+++ scripts/tuning/tune_classification_1.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+# Start base image services (Jupyter/SSH) in background
+/start.sh &
+
+# Wait for services to start
+sleep 2
+
+# Run tuning
 for model in configs/lightning/models/classification/*; do
   for pretraining in configs/lightning/pretraining/*; do
+    echo "$(date)" "$model" "$pretraining"
     python3 -m transfer_learning.train fit \
       --config configs/lightning/base.yaml \
       --config configs/lightning/task/classification_1.yaml \
       --config "$model" \
       --config configs/lightning/optimization/adamw_basic.yaml \
       --config "$pretraining" \
-      --config configs/lightning/augmentation/microscope.yaml "$@" \
-      2>&1 | tee "$DATA_ROOT/logs/$(date +%Y%m%d_%H%M%S)-$model-$pretraining.log"
+      --config configs/lightning/augmentation/microscope.yaml "$@"
     done
   done
+
+# Stop pod
+runpodctl stop pod $RUNPOD_POD_ID
--- scripts/tuning/tune_sample_sizes.sh
+++ scripts/tuning/tune_sample_sizes.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Start base image services (Jupyter/SSH) in background
+/start.sh &
+
+# Wait for services to start
+sleep 2
+
+# Run tuning
 for model in configs/lightning/models/classification/*; do
   for pretraining in configs/lightning/pretraining/*; do
     for sample_size in $(seq .1 .4 1); do
+      echo "$(date)" "$model" "$pretraining" "$sample_size"
       python3 -m transfer_learning.train fit \
         --config configs/lightning/base.yaml \
         --config configs/lightning/task/classification_1.yaml \
@@ -10,8 +18,10 @@ for model in configs/lightning/models/classification/*; do
         --config configs/lightning/optimization/adamw_basic.yaml \
         --config "$pretraining" \
         --config configs/lightning/augmentation/microscope.yaml \
-        --data.init_args.sample_size "$sample_size" "$@" \
-        2>&1 | tee "$DATA_ROOT/logs/$(date +%Y%m%d_%H%M%S)-$model-$pretraining-$sample_size.log"
+        --data.init_args.sample_size "$sample_size" "$@"
       done
     done
   done
+
+# Stop pod
+runpodctl stop pod $RUNPOD_POD_ID
