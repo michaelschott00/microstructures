39927d28e9a48e2d41d6b5c36828daef9161a245
Michael Schott <misc00001@stud.uni-saarland.de>
2026-08-11 16:44:05 +0000

    Make plot script work

--- scripts/analysis/classification_1.py
+++ scripts/analysis/classification_1.py
@@ -1,10 +1,12 @@
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 from mldb.store import RunStore
 
 store = RunStore.from_env()
 db = store.get_db()
-db.attach(names=["val_metrics"])
+db.attach(names=["val_metrics"], tags=["classification_1"])
 with db.connect() as con:
     df = con.sql(
         """
@@ -12,10 +14,11 @@ with db.connect() as con:
         from val_metrics
         """
     ).df()
-sns.barplot(
+g = sns.barplot(
     df,
     x="model",
     y="accuracy",
     hue="pretraining",
 )
-plt.show()
+g.set_title("Validation Accuracy by Model and Pretraining")
+plt.savefig("classification_results.png", bbox_inches="tight")
